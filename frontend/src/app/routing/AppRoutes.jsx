7b9492ec3ab0db878946a6af790ad844081835f6
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-11-26 16:16:01 +0530

    fix: nits

--- frontend/src/app/routing/AppRoutes.jsx
+++ frontend/src/app/routing/AppRoutes.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Navigate, Route, Routes, useSearchParams } from 'react-router-dom';
 
 import QuizPage from '../../components/QuizPage';
 import ResultsPage from '../../components/ResultsPage';
+import MasterclassNudge from '../../components/MasterclassNudge';
+import { getPathWithQueryParams, hasNudgeBeenShown } from '../../utils/url';
+
 import AdminViewPage from '../../components/admin/AdminViewPage';
 
-const AppRoutes = ({
-  quizMode,
-  onQuizProgressChange
-}) => (
-  <Routes>
-    <Route path="/" element={<Navigate to="/quiz" replace />} />
-    <Route
-      path="/quiz"
-      element={
-        <QuizPage
-          onProgressChange={onQuizProgressChange}
-          quizMode={quizMode}
-        />
-      }
-    />
-    <Route path="/results" element={<ResultsPage />} />
-    <Route path="/admin/view/response/:response_id" element={<AdminViewPage />} />
-  </Routes>
-);
+const RoutesComponent = ({ quizMode, onQuizProgressChange }) => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to={getPathWithQueryParams('/quiz')} replace />}
+      />
+      <Route
+        path="/quiz"
+        element={
+          <QuizPage
+            onProgressChange={onQuizProgressChange}
+            quizMode={quizMode}
+          />
+        }
+      />
+      <Route path="/results" element={<ResultsPage />} />
+      <Route path="/admin/view/response/:response_id" element={<AdminViewPage />} />
+    </Routes>
+  );
+};
+
+const AppRoutes = ({ quizMode, onQuizProgressChange }) => {
+  const [searchParams] = useSearchParams();
+  const nudgeId =
+    searchParams.get('show_nudge') || searchParams.get('event_id');
+
+  const [shouldShowNudge, setShouldShowNudge] = useState(false);
+
+  useEffect(() => {
+    if (nudgeId && !hasNudgeBeenShown(nudgeId)) {
+      setShouldShowNudge(true);
+    }
+  }, [nudgeId]);
+
+  return (
+    <>
+      {shouldShowNudge && <MasterclassNudge eventId={nudgeId} />}
+      <RoutesComponent {...{ quizMode, onQuizProgressChange }} />;
+    </>
+  );
+};
 
 export default AppRoutes;
