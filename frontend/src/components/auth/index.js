fe6ab5250a0e5367eed4b291662f3fdc7e59ff75
Suryam1260 <167088653+Suryam1260@users.noreply.github.com>
2025-12-02 14:52:47 +0530

    Revert "added login flow logic for cpe"

--- frontend/src/components/auth/index.js
+++
@@ -1,9 +0,0 @@
-
-export { default as SignUpForm } from './SignUpForm';
-export { default as OtpVerificationForm } from './OtpVerificationForm';
-export { default as LoginForm } from './LoginForm';
-export { default as EmailPasswordLoginForm } from './EmailPasswordLoginForm';
-export { default as BasicAuthModal } from './BasicAuthModal';
-export { default as AuthFlow } from './AuthFlow';
-
-
